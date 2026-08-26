2121ae98ba03ca4796e5611b1d6f29220b1dc0fb
Chimipupu <chiminyanyan@gmail.com>
2026-08-27 04:15:38 +0900

    PS1 BIOSチェッカーをGithubに上げて世界に共有することに決めた

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# ps1_bios_checker
+# PS1 BIOSチェッカー v0.1
+
+## 使い方
+
+### ビルド方法
+
+`gcc`でビルド
+
+```shell
+gcc -o ps1_bios_checker ps1_bios_checker.c 
+```
+
+### 実行方法
+
+例) PS1のBIOS名が `PS1_BIOS.bin`の場合
+
+```
+./ps1_bios_checker PS1_BIOS.bin
+```
+### 実行例
+
+```shell
+./ps1_bios_checker PS1_BIOS.bin
+[INFO] File Size(= 512KB) OK! Size: 524288 Byte
+[INFO] Calc CRC32: 0xEC541CD0
+[INFO] CRC32 Match! CRC32: 0xEC541CD0
+[INFO] BIOS Type: ps-40j
+[INFO] PS1 Type: SCPH-7000
+```
+
+## チェック対象のBIOS
+
+| 型番 | BIOS名 | BIOS CRC32 |
+| --- | --- | --- |
+| SCPH-1000 | ps-10j | 0x3B601FC8 |
+| SCPH-3000 | ps-11j | 0x3539DEF6 |
+| SCPH-3500 | ps-21j | 0xBC190209 |
+| SCPH-5500 | ps-30j | 0xFF3EEB8C |
+| SCPH-7000 | ps-40j | 0xEC541CD0 |
+| SCPH-100 (PS one) | psone-43j | 0xF2AF798B |
---
+++ ps1_bios_checker.c
@@ -0,0 +1,142 @@
+/**
+ * @file ps1_bios_checker.c
+ * @author Chimipupu(https://github.com/Chimipupu)
+ * @brief PS1 BIOSのチェッカー
+ * @version 0.1
+ * @date 2026-08-27
+ * @copyright Copyright (c) 2026 Chimipupu All Rights Reserved.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+// ---------------------------------------------------
+typedef struct {
+    uint32_t crc32;             // CRC32 期待値
+    const char *p_ps1_name;     // PS1の型番名
+    const char *p_bios_name;    // BIOS名
+} bios_crc32_t;
+
+// BIOSのCRC32、PS1の型番名、BIOS名のテーブル
+const bios_crc32_t g_bios_crc32_tbl[] = {
+    { 0x3B601FC8, "SCPH-1000", "ps-10j"    },
+    { 0x3539DEF6, "SCPH-3000", "ps-11j"    },
+    { 0xBC190209, "SCPH-3500", "ps-21j"    },
+    { 0xFF3EEB8C, "SCPH-5500", "ps-30j"    },
+    { 0xEC541CD0, "SCPH-7000", "ps-40j"    },
+    { 0xF2AF798B, "SCPH-100",  "psone-43j" }
+};
+const uint8_t BIOS_CRC32_TBL_CNT = sizeof(g_bios_crc32_tbl) / sizeof(g_bios_crc32_tbl[0]);
+
+static uint32_t _calc_crc32(const uint8_t *p_buf, size_t size);
+static void _check_bios_file(const char *p_filepath);
+// ---------------------------------------------------
+// [Static]
+
+// CRC32の計算関数
+static uint32_t _calc_crc32(const uint8_t *p_buf, size_t size)
+{
+    uint32_t crc;
+    size_t i;
+    size_t j;
+
+    crc = 0xFFFFFFFF;
+    for (i = 0; i < size; i++)
+    {
+        crc ^= p_buf[i];
+        for (j = 0; j < 8; j++) {
+            if (crc & 1) {
+                crc = (crc >> 1) ^ 0xEDB88320;
+            } else {
+                crc = (crc >> 1);
+            }
+        }
+    }
+
+    return ~crc;
+}
+
+// BIOSファイルを読み込んでチェックする関数
+static void _check_bios_file(const char *p_filepath)
+{
+    FILE *p_fp;
+    uint8_t *p_buf;
+    long file_size;
+    size_t read_size;
+    uint32_t calc_crc;
+    size_t i;
+    bool is_match;
+
+    is_match = false;
+    p_fp = fopen(p_filepath, "rb");
+
+    if (p_fp == NULL) {
+        printf("[INFO] Fail. File Open: %s\n", p_filepath);
+        return;
+    }
+
+    // ファイルサイズの取得
+    fseek(p_fp, 0, SEEK_END);
+    file_size = ftell(p_fp);
+    fseek(p_fp, 0, SEEK_SET);
+
+    // NOTE: PS1のBIOSは通常512KB (524288 bytes)
+    if (file_size != 512 * 1024) {
+        printf("\33[33m[WARN] File Size Not 512KB (Size: %ld Byte)\n\33[0m", file_size);
+    } else {
+        printf("\33[32m[INFO] File Size(= 512KB) OK! Size: %ld Byte\n", file_size);
+    }
+
+    p_buf = (uint8_t *)malloc((size_t)file_size);
+    if (p_buf == NULL) {
+        printf("\33[31m[ERROR] Memory Malloc FAIL\n\33[0m");
+        fclose(p_fp);
+        return;
+    }
+
+    read_size = fread(p_buf, 1, (size_t)file_size, p_fp);
+    if (read_size != (size_t)file_size) {
+        printf("\33[31m[ERROR] BIOS File Read FAIL\n\33[0m");
+        free(p_buf);
+        fclose(p_fp);
+        return;
+    }
+    fclose(p_fp);
+
+    // CRC32の計算
+    calc_crc = _calc_crc32(p_buf, (size_t)file_size);
+    free(p_buf);
+    printf("\33[32m[INFO] Calc CRC32: 0x%08X\n\33[0m", calc_crc);
+
+    // CRC32テーブルの検索
+    for (i = 0; i < BIOS_CRC32_TBL_CNT; i++)
+    {
+        if (g_bios_crc32_tbl[i].crc32 == calc_crc) {
+            printf("\33[32m[INFO] CRC32 Match! CRC32: 0x%08X\n\33[0m", g_bios_crc32_tbl[i].crc32);
+            printf("\33[32m[INFO] BIOS Type: %s\n\33[0m", g_bios_crc32_tbl[i].p_bios_name);
+            printf("\33[32m[INFO] PS1 Type: %s\n\33[0m", g_bios_crc32_tbl[i].p_ps1_name);
+            is_match = true;
+            break;
+        }
+    }
+
+    if (!is_match) {
+        printf("\33[32m[INFO] Not Match All PS1 BIOS CRC32\n\33[0m");
+    }
+}
+// ---------------------------------------------------
+// [メイン]
+int main(int argc, char **p_argv)
+{
+    if (argc < 2) {
+        printf("\33[31m[ERROR] Please, PS1 BIOS File Path\n\33[0m");
+        return 1;
+    }
+
+    _check_bios_file(p_argv[1]);
+
+    return 0;
+}
+// ---------------------------------------------------
